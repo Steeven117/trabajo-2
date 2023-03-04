2fd0543627549a6cbbac6a658147d2074972390e
Steeven117 <walter2234519@correo.uis.edu.co>
2023-03-03 20:05:19 -0500

    trabajo

---
+++ trabajo2.py
@@ -0,0 +1,20 @@
+print("-------------------------------------------------")
+print("---------------A ES MAYOR QUE B------------------")
+print("---------------B ES MENOR QUE A------------------")
+print("-------------------------------------------------")
+
+#input
+
+A= int(input("Digite el valor de A: "))
+B= int(input("Digite el valor de B: "))
+#Procesing
+
+if A>B : 
+    rta = " A es mayor"
+
+else:
+    rta = " B es mayor"
+
+
+
+print("El valor " + str(rta))
